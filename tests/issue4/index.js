c87f9ac33b2533752d6b4304903989532541f689
mutou <mutou@gaoding.com>
2021-08-02 20:47:51 +0800

    optimze: 优化解析相关空格出现性能问题

---
+++ tests/issue4/index.js
@@ -0,0 +1,14 @@
+const sqlParser = require('../../index');
+const fs = require('fs');
+
+(async () => {
+    const sqlFilename = './sql.txt';
+    const content = fs.readFileSync(sqlFilename).toString();
+    try {
+        const result = sqlParser.parse(content);
+        console.log(JSON.stringify(result, null, '    '));
+        console.log('result count: ', result.length);
+    } catch (err) {
+        console.log('err: ', err.message.substring(0, 200));
+    }
+})();
